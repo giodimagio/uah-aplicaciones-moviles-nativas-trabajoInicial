5b5eb5f426ab6518fe9a314ed00f8c3590eb1f42
giodimagio <giodimagio@gmail.com>
2023-04-05 23:28:58 +0200

    [MainActivity] Nueva lista vacía y botón flotante que nos lleva a Activity Create al hacer click

--- app/src/main/java/com/example/trabajoinicial/MainActivity.java
+++ app/src/main/java/com/example/trabajoinicial/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.trabajoinicial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton addFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.listRecyclerView);
+        addFloatingActionButton = findViewById(R.id.addFloatingActionButton);
+        addFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
