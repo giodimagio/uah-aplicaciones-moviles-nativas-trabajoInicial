0e4a8c4a602469b0935bd7ae059c0e3d77f8eabe
giodimagio <giodimagio@gmail.com>
2023-04-09 18:29:15 +0200

    [MainActivity] Actualizacion de datos en varios puntos del Activity-lifecycle y algún refactor

--- app/src/main/java/com/example/trabajoinicial/MainActivity.java
+++ app/src/main/java/com/example/trabajoinicial/MainActivity.java
@@ -15,13 +15,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    FloatingActionButton addFloatingActionButton;
+    private RecyclerView recyclerView;
 
-    AsignaturasSQLiteHelper asignaturasdb;
-    ArrayList<String> nombre_asignatura;
-    ArrayList<Integer> id_asignatura, calificacion_asignatura;
-    AsignaturasCustomAdapter asignaturasCustomAdapter;
+    private AsignaturasSQLiteHelper asignaturasdb;
+    private ArrayList<String> nombre_asignatura;
+    private ArrayList<Integer> id_asignatura, calificacion_asignatura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,36 +27,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.listRecyclerView);
-        addFloatingActionButton = findViewById(R.id.addFloatingActionButton);
-        addFloatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CreateActivity.class);
-                startActivity(intent);
-            }
+        FloatingActionButton addFloatingActionButton = findViewById(R.id.addFloatingActionButton);
+        addFloatingActionButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CreateActivity.class);
+            startActivity(intent);
         });
 
         asignaturasdb = new AsignaturasSQLiteHelper(MainActivity.this);
+
         id_asignatura = new ArrayList<>();
         nombre_asignatura = new ArrayList<>();
         calificacion_asignatura = new ArrayList<>();
-
-        mostrarTabla();
-
-        asignaturasCustomAdapter = new AsignaturasCustomAdapter(MainActivity.this,
-                id_asignatura, nombre_asignatura, calificacion_asignatura);
-
-        recyclerView.setAdapter(asignaturasCustomAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
     }
 
-    public void mostrarTabla() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        clearArrayLists();
+        loadData();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        clearArrayLists();
+        loadData();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        clearArrayLists();
+    }
+
+    private void clearArrayLists() {
+        id_asignatura.clear();
+        nombre_asignatura.clear();
+        calificacion_asignatura.clear();
+    }
+
+    private void loadData() {
+        clearArrayLists();
+
         Cursor cursor = asignaturasdb.leerTabla();
         while (cursor.moveToNext()) {
             id_asignatura.add(cursor.getInt(0));
             nombre_asignatura.add(cursor.getString(1));
             calificacion_asignatura.add(cursor.getInt(2));
         }
+
+        AsignaturasCustomAdapter asignaturasCustomAdapter = new AsignaturasCustomAdapter(MainActivity.this,
+                id_asignatura, nombre_asignatura, calificacion_asignatura);
+
+        recyclerView.setAdapter(asignaturasCustomAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
     }
 
 }
