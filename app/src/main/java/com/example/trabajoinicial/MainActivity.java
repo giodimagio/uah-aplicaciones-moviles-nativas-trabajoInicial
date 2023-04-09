1e5f7c00278f5dd12a162c1daef1374be79affb9
giodimagio <giodimagio@gmail.com>
2023-04-09 16:49:51 +0200

    [MainActivity] Lógica para mostrar la tabla a partir de los datos de la BBDD SQLite

--- app/src/main/java/com/example/trabajoinicial/MainActivity.java
+++ app/src/main/java/com/example/trabajoinicial/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.trabajoinicial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     FloatingActionButton addFloatingActionButton;
+
+    AsignaturasSQLiteHelper asignaturasdb;
+    ArrayList<String> nombre_asignatura;
+    ArrayList<Integer> id_asignatura, calificacion_asignatura;
+    AsignaturasCustomAdapter asignaturasCustomAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +37,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        asignaturasdb = new AsignaturasSQLiteHelper(MainActivity.this);
+        id_asignatura = new ArrayList<>();
+        nombre_asignatura = new ArrayList<>();
+        calificacion_asignatura = new ArrayList<>();
+
+        mostrarTabla();
+
+        asignaturasCustomAdapter = new AsignaturasCustomAdapter(MainActivity.this,
+                id_asignatura, nombre_asignatura, calificacion_asignatura);
+
+        recyclerView.setAdapter(asignaturasCustomAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
     }
+
+    public void mostrarTabla() {
+        Cursor cursor = asignaturasdb.leerTabla();
+        while (cursor.moveToNext()) {
+            id_asignatura.add(cursor.getInt(0));
+            nombre_asignatura.add(cursor.getString(1));
+            calificacion_asignatura.add(cursor.getInt(2));
+        }
+    }
+
 }
