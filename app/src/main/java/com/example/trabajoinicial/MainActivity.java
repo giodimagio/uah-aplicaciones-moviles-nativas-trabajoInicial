eca0c492ec78d1bb89bca8166656682681e1fe77
giodimagio <giodimagio@gmail.com>
2023-04-16 21:27:58 +0200

    [Splash] Configuraciones Gradle, Manifest, etc para SplashScreen API

--- app/src/main/java/com/example/trabajoinicial/MainActivity.java
+++ app/src/main/java/com/example/trabajoinicial/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.trabajoinicial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -23,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
